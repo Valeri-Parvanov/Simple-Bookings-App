d338199508a4f67781c45a137e69acb1d494b031
Valeri Parvanov <valyo.parvanov@gmail.com>
2025-10-03 20:10:23 +0200

    chore: merge remote initial files into local project

--- src/main/java/app/promocode/model/PromoCode.java
+++ src/main/java/app/promocode/model/PromoCode.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,7 +17,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "promocodes")
 public class PromoCode {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
